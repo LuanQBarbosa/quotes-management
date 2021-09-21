fba84a89ea692f85c9e4878e76d92e1ba6b5b450
Luan Barbosa <luanqbarbosa@gmail.com>
2021-09-21 16:12:31 -0300

    Link to quote details from quotes list and HighlightedQuote component use

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,15 +1,36 @@
 import { Fragment } from "react";
 import { Route, useParams } from "react-router";
 
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
 import Comments from '../components/comments/Comments';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Luan',
+    text: 'Learning React is fun!'
+  },
+  {
+    id: 'q2',
+    author: 'Lucas',
+    text: 'Learning React is great!'
+  }
+]
 
 const QuoteDetail = () => {
   const { quoteId } = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === quoteId);
+
+  if (!quote) {
+    return (
+      <p>No quote found!</p>
+    );
+  }
   
   return (
     <Fragment>
-      <h1>Quote Detail Page</h1>
-      <p>{quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
