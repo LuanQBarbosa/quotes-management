367633413b5264f7a8558d2b32fbc21784c3936f
Luan Barbosa <luanqbarbosa@gmail.com>
2021-09-22 09:42:55 -0300

    Storing and getting quote comments on database

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,14 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import { useParams } from 'react-router-dom';
 
-import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
+import CommentsList from './CommentsList';
+import LoadingSpinner from '../UI/LoadingSpinner';
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
+import classes from './Comments.module.css';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
+  const { quoteId } = useParams();
+
+  const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHandler = useCallback(() => {
+    sendRequest(quoteId);
+  }, [sendRequest, quoteId]);
+
+  let comments;
+  
+  if (status === 'pending') {
+    comments = (
+      <div className="centered">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  if (status === 'completed' && (loadedComments && loadedComments.length > 0)) {
+    comments = <CommentsList comments={loadedComments} />;
+  }
+
+  if (status === 'completed' && (!loadedComments || loadedComments.length === 0)) {
+    comments = <p className="centered">No comments were added yet!</p>
+  }
   
   return (
     <section className={classes.comments}>
@@ -18,8 +52,8 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm />}
-      <p>Comments...</p>
+      {isAddingComment && <NewCommentForm quoteId={quoteId} onAddedComment={addedCommentHandler} />}
+      {comments}
     </section>
   );
 };
